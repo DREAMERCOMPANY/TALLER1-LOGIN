88f798fe14193f456270d45d0472b341556b575d
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2024-07-19 18:38:00 -0500

    Change directory name

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jhon9\Documents\Platzi\Desarrollo Web\Python\TALLER1-LOGIN\venv
+set VIRTUAL_ENV=C:\Users\jhon9\Documents\Platzi\Desarrollo Web\Python\TALLER1-j.ramirez55\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
